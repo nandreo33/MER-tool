8e92522c86cc3f9a882edd9e8e60ca2cbbe58705
Nathan Andreo <nathan.andreo33@gmail.com>
2018-06-14 17:44:01 -0400

    v0.6.3
    remove script.m

--- script.m
+++
@@ -1,8 +0,0 @@
-tF = dir('E:/nandreo33/Downloads/MER_ForNathanMax/MER_ForNathanMax/First_Last/GPi Left/**/Waveform*.apm');
-folder = cell(1,size(tF,1));
-
-for i = 1:size(tF,1)
-    folder(i) = {tF(i).folder};
-end
-
-StF = (folder);
